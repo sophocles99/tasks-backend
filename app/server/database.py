01bba77875211f407ac2187d65bf86066d56d115
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2024-05-18 16:39:16 +0100

    add helper to convert MongoDB document into dict

--- app/server/database.py
+++ app/server/database.py
@@ -5,3 +5,15 @@ MONGO_URI = "mongodb://localhost:27017"
 client = motor_asyncio.AsyncIOMotorClient(MONGO_URI)
 db = client.students
 student_collection = db.get_collection("students_collection")
+
+
+# helpers
+def student_helper(student) -> dict:
+    return {
+        "id": str(student["_id"]),
+        "fullname": student["fullname"],
+        "email": student["email"],
+        "course_of_study": student["course_of_study"],
+        "year": student["year"],
+        "gpa": student["gpa"],
+    }
