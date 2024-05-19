c9644cc30485a015be6cc21e1238507a97cdb0ae
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2024-05-19 16:46:42 +0100

    add return types to path operation functions

--- app/server/routes/student_router.py
+++ app/server/routes/student_router.py
@@ -12,13 +12,13 @@ router = APIRouter()
 
 
 @router.get("/", response_description="Students retrieved from database")
-async def get_students():
+async def get_students() -> list[Student]:
     students = await retrieve_students()
     return students
 
 
 @router.get("/{id}", response_description="Student retrieved from database")
-async def get_student(id: str):
+async def get_student(id: str) -> Student:
     student = await retrieve_student(id)
     if not student:
         raise HTTPException(status_code=404, detail="No student found with that id")
@@ -26,21 +26,21 @@ async def get_student(id: str):
 
 
 @router.post("/", response_description="Student added into database", status_code=201)
-async def post_student(student: Student):
+async def post_student(student: Student) -> Student:
     student_dict = student.model_dump()
     new_student = await create_student(student_dict)
     return new_student
 
 
 @router.patch("/{id}", response_description="Student updated")
-async def patch_student(id: str, student_update: StudentUpdate):
+async def patch_student(id: str, student_update: StudentUpdate) -> Student:
     update_dict = {k: v for k, v in student_update if v is not None}
     updated_student = await update_student(id, update_dict)
     return updated_student
 
 
 @router.delete("/{id}", response_description="Student deleted", status_code=204)
-async def delete_student(id: str):
+async def delete_student(id: str) -> None:
     delete_success = await remove_student(id)
     if not delete_success:
         raise HTTPException(status_code=404, detail="No student found with that id")
