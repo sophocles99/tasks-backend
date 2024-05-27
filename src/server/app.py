dd4fadc1498ad58a46f9a7ab7b292b77e8f5c466
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2024-05-27 11:31:00 +0100

    add CORS middleware

--- src/server/app.py
+++ src/server/app.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from server.routes.student_router import student_router
 from server.routes.todo_router import todo_router
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(student_router, tags=["students"], prefix="/students")
 app.include_router(todo_router, tags=["todos"], prefix="/todos")
 
