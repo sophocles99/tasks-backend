a42b1a22cdcce31a2c0f1eebc6ae11f7cd422680
David McQueen <david.mcqueen@krakenflex.com>
2024-12-06 19:04:34 +0000

    start again

--- src/server/database.py
+++
@@ -1,9 +0,0 @@
-from motor import motor_asyncio
-
-MONGO_URI = "mongodb://localhost:27017"
-
-client = motor_asyncio.AsyncIOMotorClient(MONGO_URI)
-db = client.tasks
-
-student_collection = db.get_collection("students_collection")
-task_collection = db.get_collection("task_collection")
