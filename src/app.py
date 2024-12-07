cfa57041b66bd763c7850de48618ce652e76d03a
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2024-12-07 23:17:40 +0000

    add read_task operation

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from sqlmodel import Session, select
 
 from database import create_tables, engine
@@ -31,3 +31,12 @@ def read_tasks():
     with Session(engine) as session:
         tasks = session.exec(select(Task)).all()
         return tasks
+
+
+@app.get("/tasks/{task_id}", response_model=TaskPublic)
+def read_task(task_id: int):
+    with Session(engine) as session:
+        task = session.get(Task, task_id)
+        if not task:
+            raise HTTPException(status_code=404, detail="Task not found")
+        return task
