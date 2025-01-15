b06fe8071d4542798051b701558f19ee89da2208
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2025-01-15 22:17:13 +0000

    add uvicorn log_config=None to disable default log handling and allow print statements

--- tasks_backend/main.py
+++ tasks_backend/main.py
@@ -2,7 +2,7 @@ import uvicorn
 
 
 def main():
-    uvicorn.run("tasks_backend.app:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("tasks_backend.app:app", host="localhost", port=8000, reload=True, log_config=None)
 
 
 if __name__ == "__main__":
