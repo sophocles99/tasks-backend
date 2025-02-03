9031fc554c4ff888734b8f42bdf7e3fff5353cb9
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2025-02-03 13:51:24 +0000

    read environment variable to determine uvicorn reload

--- tasks_backend/run.py
+++ tasks_backend/run.py
@@ -1,7 +1,11 @@
+import os
+
 import uvicorn
 
+RELOAD = os.environ.get("RELOAD", False)
+
 def run_server():
-    uvicorn.run("tasks_backend.app:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("tasks_backend.app:app", host="localhost", port=8000, reload=RELOAD)
 
 
 if __name__ == "__main__":
