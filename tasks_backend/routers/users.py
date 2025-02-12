b040ce2fb7a564107f732a604562109931a96ddd
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2025-02-12 22:22:52 +0000

    allow user to update password, protect some users routes

--- tasks_backend/routers/users.py
+++ tasks_backend/routers/users.py
@@ -3,10 +3,11 @@ from uuid import UUID
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlmodel import Session, select
 
-from tasks_backend.auth import hash_password
+from tasks_backend.auth import get_current_user, hash_password
 from tasks_backend.db import get_session
 from tasks_backend.models.categories import create_default_categories
 from tasks_backend.models.users import User, UserCreate, UserPublic, UserUpdate, get_user_or_raise_404
+from tasks_backend.utils.utils import get_current_utc_time
 
 router = APIRouter(prefix="/users")
 
@@ -26,22 +27,26 @@ def create_user(user_create: UserCreate, session: Session = Depends(get_session)
 
 
 @router.get("", response_model=list[UserPublic])
-def read_users(session: Session = Depends(get_session)):
+def read_users(_=Depends(get_current_user), session: Session = Depends(get_session)):
     users = session.exec(select(User)).all()
     return users
 
 
-@router.get("/{user_id}", response_model=UserPublic)
-def read_user(user_id: UUID, session: Session = Depends(get_session)):
-    user = get_user_or_raise_404(user_id, session)
-    return user
+@router.get("", response_model=UserPublic)
+def read_user(current_user: User = Depends(get_current_user), session: Session = Depends(get_session)):
+    return current_user
 
 
 @router.patch("/{user_id}", response_model=UserPublic)
-def update_user(user_id: UUID, user_update: UserUpdate, session: Session = Depends(get_session)):
-    user = get_user_or_raise_404(user_id, session)
+def update_user(
+    user_update: UserUpdate, current_user: User = Depends(get_current_user), session: Session = Depends(get_session)
+):
+    user = current_user
+    if user_update.password:
+        user.hashed_password = hash_password(user_update.password)
     user_update_data = user_update.model_dump(exclude_unset=True)
     user.sqlmodel_update(user_update_data)
+    user.updated_at = get_current_utc_time()
     session.add(user)
     session.commit()
     session.refresh(user)
