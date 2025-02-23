b298b988ce11af7fd327dc1cfb492652632316c6
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2025-02-23 05:59:14 +0000

    update deployment script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -3,13 +3,21 @@
 set -e
 
 PROJECT_ROOT=$(dirname "$(dirname "$(readlink -f "$0")")")
-REQUIREMENTS_FILEPATH="lambda-requirements/requirements.txt"
+DEPLOYMENT_FOLDER="$PROJECT_ROOT/deployment"
+REQUIREMENTS_FOLDER="$DEPLOYMENT_FOLDER/requirements"
+REQUIREMENTS_FILEPATH="$REQUIREMENTS_FOLDER/requirements.txt"
 
-cd "$PROJECT_ROOT/deployment"
+[ ! -d "$REQUIREMENTS_FOLDER" ] && mkdir -p "$REQUIREMENTS_FOLDER"
+
+cd "$DEPLOYMENT_FOLDER"
 
 pip install pip-tools
-pip-compile ../pyproject.toml --output-file="$REQUIREMENTS_FILEPATH" --verbose
+if ! pip-compile "$PROJECT_ROOT/pyproject.toml" --output-file="$REQUIREMENTS_FILEPATH" --verbose; then
+    echo "Failed to compile requirements"
+    exit 1
+fi
+
 echo "$PROJECT_ROOT" >> "$REQUIREMENTS_FILEPATH"
 
-sam build --debug --template ../template.yaml
+sam build --debug --template "$PROJECT_ROOT/template.yaml"
 sam deploy
