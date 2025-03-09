1869e259bca1ae100bc5940e65aaed873e6934ac
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2025-03-09 22:02:10 +0000

    log invocation count and event in handler, add message to root endpoint

--- tasks_backend/app.py
+++ tasks_backend/app.py
@@ -1,3 +1,5 @@
+import logging
+import os
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, Request
@@ -6,6 +8,8 @@ from mangum import Mangum
 from tasks_backend.db import create_tables
 from tasks_backend.routers import auth, categories, tasks, users
 
+_invocation_count = 0
+
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
@@ -13,16 +17,34 @@ async def lifespan(_: FastAPI):
     yield
 
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=lifespan, root_path="/Prod")
+
 
 @app.get("/")
 async def root(request: Request):
-    print(request.scope.get("aws.event", {}))
-    return {"message": "Hello World"}
+    logging.info("Running root operation function")
+    if "AWS_LAMBDA_FUNCTION_NAME" in os.environ:
+        message = (
+            f"FastAPI running on AWS Lambda in region {os.getenv('AWS_REGION')}, "
+            f"using runtime environment {os.getenv('AWS_EXECUTION_ENV')}"
+        )
+    else:
+        message = "FastAPI running locally"
+    return {"message": message}
+
 
 app.include_router(auth.router)
 app.include_router(categories.router)
 app.include_router(tasks.router)
 app.include_router(users.router)
 
-lambda_handler = Mangum(app)
+
+def lambda_handler(event, context):
+    global _invocation_count
+    _invocation_count += 1
+    logging.info(f"Invocation count: {_invocation_count}")
+    logging.info(event)
+    asgi_handler = Mangum(app, lifespan="auto")
+    response = asgi_handler(event, context)
+
+    return response
